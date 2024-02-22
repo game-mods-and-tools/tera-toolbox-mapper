f5fa3d8fb0303b8448a3f529867e44c70adaffe2
reimu <r2hkri@gmail.com>
2024-02-22 15:18:38 -0600

    add more data

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-let inCombat = false;
-
 exports.NetworkMod = class {
 	constructor(mod) {
 		this.lastReceivedCode = -1;
 		
 		mod.hook("*", "raw", (code, buf, incoming, fake) => {
 			if (code === this.lastReceivedCode) return;
+
+			const name = mod.dispatch.protocolMap.code.get(code);
 			
-			console.log({ code, name: mod.dispatch.protocolMap.code.get(code) });
+			let data = null;
+			if (name) {
+				try {
+					data = mod.dispatch.fromRaw(name, "*", buf);
+				} catch {
+					// missing def
+				}
+			}
+			
+			console.log({ code, name, data });
 			this.lastReceivedCode = code;
 		});
 	}
